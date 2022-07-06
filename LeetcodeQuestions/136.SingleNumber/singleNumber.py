58c64375ed0952e16ef86d763a84a0777871d122
a42f9b2c34582681d2ef4f51ef75070144258469 <lykawangg@gmail.com>
2022-07-05 17:56:25 -0700

    test Git email

--- LeetcodeQuestions/136.SingleNumber/singleNumber.py
+++ LeetcodeQuestions/136.SingleNumber/singleNumber.py
@@ -20,3 +20,16 @@ def singleNumber(nums):
 
 # runtime beats 29.44%
 # memory usage beats 98.41%
+
+
+
+
+def singleNumber(self, nums: List[int]) -> int:
+    nums.sort()
+    i = 0
+    while i < len(nums):
+        if i == len(nums) - 1:
+            return nums[i]
+        elif nums[i] != nums[i + 1]:
+            return nums[i]
+        i = i + 2
