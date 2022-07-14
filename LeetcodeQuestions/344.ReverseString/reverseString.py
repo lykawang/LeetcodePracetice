1d194125523c12845f3e84c5efe3e1ce0a138bc0
a42f9b2c34582681d2ef4f51ef75070144258469 <lykawangg@gmail.com>
2022-07-13 22:19:17 -0700

    did Leetcode question Reverse String

---
+++ LeetcodeQuestions/344.ReverseString/reverseString.py
@@ -0,0 +1,24 @@
+def reverseString(s):
+    """
+    :type s: List[str]
+    :rtype: None Do not return anything, modify s in-place instead.
+    """
+    n = len(s)
+    t = 0
+    while t < n // 2:
+        s[t], s[n - 1 - t] = s[n - 1 - t], s[t]
+        t += 1
+
+    # shortcut:
+    # s[:]=s[::-1]
+    # list[< start >: < stop >: < step >]
+
+
+s = ["h", "e", "l", "l", "o"]
+reverseString(s)
+for x in s:
+    print(x)
+
+# runtime beats 28.82%
+
+# memory usage beats 77.73%
